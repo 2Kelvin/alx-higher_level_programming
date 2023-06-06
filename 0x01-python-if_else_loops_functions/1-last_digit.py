bf2af708c72c1ef96212b60b1c15872a98604cf6
2Kelvin <rocketmancodes02@gmail.com>
2023-06-06 09:45:28 -0700

    Add if clause for negative values

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,6 +3,8 @@ import random
 number = random.randint(-10000, 10000)
 numStr = str(number)
 lastDigit = int(numStr[-1])
+if number < 0:
+    lastdigit = -lastDigit
 if lastDigit > 5:
     print(f"Last digit of {number} is {lastDigit} and is greater than 5")
 elif lastDigit == 0:
