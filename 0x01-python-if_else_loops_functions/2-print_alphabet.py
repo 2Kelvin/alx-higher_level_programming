1a02193acac803d13bdf5a1d4b28815d87d5e732
2Kelvin <rocketmancodes02@gmail.com>
2023-06-06 10:37:25 -0700

    Remove comments

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-# ASCII code for small letters => 97 - 122
-# chr() converts ascii number to the respective alphabet
-# end="" replaces print's newline with nothing
 for alpha in range(97, 123):
     print("{}".format(chr(alpha)), end="")
