3dca04a3555086f5fd78932d2048826557a453b3
2Kelvin <rocketmancodes02@gmail.com>
2023-06-06 10:27:08 -0700

    Add string format style

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -3,4 +3,4 @@
 for letter in range(97, 123):
     # chr() converts ascii number to the respective alphabet
     # end="" replaces print's newline with nothing
-    print(chr(letter), end="")
+    print("{}".format(chr(letter)), end="")
