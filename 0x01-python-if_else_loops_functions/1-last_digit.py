64d18727048c4214e3e89762df0186b9e09952ba
2Kelvin <rocketmancodes02@gmail.com>
2023-06-06 15:51:48 -0700

    Add int to umber in string

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,9 +6,9 @@ lastDigit = int(numStr[-1])
 if number < 0:
     lastdigit = -lastDigit
 if lastDigit > 5:
-    print(f"Last digit of {number} is {lastDigit} and is greater than 5")
+    print(f"Last digit of {number:d} is {lastDigit:d} and is greater than 5")
 elif lastDigit == 0:
-    print(f"Last digit of {number} is {lastDigit} and is 0")
+    print(f"Last digit of {number:d} is {lastDigit:d} and is 0")
 elif lastDigit < 6 and lastDigit != 0:
-    print(f"Last digit of {number} is {lastDigit} "
+    print(f"Last digit of {number:d} is {lastDigit:d} "
           "and is less than 6 and not 0")
