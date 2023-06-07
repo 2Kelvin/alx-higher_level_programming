bee3fa451cf87d7a208051abed5c5d2afd6cb8c8
2Kelvin <rocketmancodes02@gmail.com>
2023-06-07 07:29:30 -0700

    Fix extra character before z

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for al in range(123, 96, -1):
+for al in range(122, 96, -1):
     if al % 2 == 0:
         cs = 0
-    else:
+    elif al % 2 != 0:
         cs = 32
     print("{}".format(chr(al - cs)), end="")
