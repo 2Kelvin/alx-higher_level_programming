b502fd38cf3f26a69ecd7a2cb2984d9191f61cf8
2Kelvin <rocketmancodes02@gmail.com>
2023-06-07 04:17:25 -0700

    Print in reverse & fix missing equal operator

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for al in range(97, 123):
+for al in range(123, 96, -1):
     if al % 2 == 0:
         print(chr(al), end="")
     elif al % 2 != 0:
-        al al - 32
+        al = al - 32
         print(chr(al), end="")
