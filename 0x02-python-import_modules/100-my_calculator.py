5207111adc43073a5a2a04371a274093a43118a0
2Kelvin <rocketmancodes02@gmail.com>
2023-06-09 08:57:07 -0700

    Fix pycode err + variable names

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,24 +4,24 @@ from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
     args = len(sys.argv) - 1
     if args == 3:
-        arg1 = int(sys.argv[1])
+        a = int(sys.argv[1])
         op = sys.argv[2]
-        arg2 = int(sys.argv[3])
+        b = int(sys.argv[3])
         if op == '+':
-            print(f"{arg1:d} + {arg2:d} = {(add(arg1, arg2):d)}")
+            print(f"{a:d} + {b:d} = {add(a, b):d}")
             sys.exit(0)
         elif op == '-':
-            print(f"{arg1:d} - {arg2:d} = {(sub(arg1, arg2):d)}")
+            print(f"{a:d} - {b:d} = {sub(a, b):d}")
             sys.exit(0)
         elif op == '*':
-            print(f"{arg1:d} * {arg2:d} = {(mul(arg1, arg2):d)}")
+            print(f"{a:d} * {b:d} = {mul(a, b):d}")
             sys.exit(0)
         elif op == '/':
-            print(f"{arg1:d} / {arg2:d} = {(div(arg1, arg2):d)}")
+            print(f"{a:d} / {b:d} = {div(a, b):d}")
             sys.exit(0)
         else:
             print("Unknown operator. Available operators: +, -, * and /")
-	    sys.exit(1)
+            sys.exit(1)
     else:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
