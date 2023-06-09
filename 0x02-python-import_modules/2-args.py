6dc27ff370a65d1019331b241bc0dfc6983f803a
2Kelvin <rocketmancodes02@gmail.com>
2023-06-08 17:57:24 -0700

    Add import exception guard

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-import sys
-argC = len(sys.argv)
-if argC == 1:
-    print("0 arguments.")
-elif argC == 2:
-    print(f"1 argument:\n{argC - 1}: {sys.argv[1]}")
-else:
-    print(f"{(argC - 1):d} arguments:")
-    for arg in range(1, argC):
-        print(f"{arg:d}: {sys.argv[arg]}")
+if __name__ == "__main__":
+    import sys
+    argC = len(sys.argv)
+    if argC == 1:
+        print("0 arguments.")
+    elif argC == 2:
+        print(f"1 argument:\n{argC - 1}: {sys.argv[1]}")
+    else:
+        print(f"{(argC - 1):d} arguments:")
+        for arg in range(1, argC):
+            print(f"{arg:d}: {sys.argv[arg]}")
