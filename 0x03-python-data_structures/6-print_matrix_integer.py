d64ee22b98b3ec0488b927f4de8e35f36fd36d6b
2Kelvin <rocketmancodes02@gmail.com>
2023-06-12 05:23:33 -0700

    Remove if statement

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix:
-        for m in matrix:
-            for e in m:
-                if e == 3 or e == 6 or e == 9:
-                    print("{:d}".format(e), end="")
-                else:
-                    print("{:d}".format(e), end=" ")
-            print()
+    for m in matrix:
+        for e in m:
+            if e == 3 or e == 6 or e == 9:
+                print("{:d}".format(e), end="")
+            else:
+                print("{:d}".format(e), end=" ")
+        print()
