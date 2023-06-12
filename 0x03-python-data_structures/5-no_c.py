745a61b7ef2b05c1a5b81b135056ea5ab4b01ffa
2Kelvin <rocketmancodes02@gmail.com>
2023-06-12 04:46:12 -0700

    Use while loop

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
     for i in range(len(my_string)):
-        if i < len(my_string):
+        while i < len(my_string):
             if my_string[i] == "c" or my_string[i] == "C":
-                new_string = my_string[:i] + my_string[(i + 1):]
-                break
-    return new_string
+                my_string = my_string[:i] + my_string[(i + 1):]
+            i = i + 1
+    return my_string
