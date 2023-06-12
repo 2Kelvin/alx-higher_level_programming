494c11a7342cd9fb141f7830df805f9489d563bb
2Kelvin <rocketmancodes02@gmail.com>
2023-06-12 05:21:50 -0700

    Add integer format

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,7 @@ def print_matrix_integer(matrix=[[]]):
         for m in matrix:
             for e in m:
                 if e == 3 or e == 6 or e == 9:
-                    print("{}".format(e), end="")
+                    print("{:d}".format(e), end="")
                 else:
-                    print("{}".format(e), end=" ")
+                    print("{:d}".format(e), end=" ")
             print()
