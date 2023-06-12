d27cc51c45eb378896bdff6fa8eddd8ab0058ad3
2Kelvin <rocketmancodes02@gmail.com>
2023-06-12 05:28:20 -0700

    Edit if statement

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for m in matrix:
         for e in m:
-            if e == 3 or e == 6 or e == 9:
+            if e == m[-1]:
                 print("{:d}".format(e), end="")
             else:
                 print("{:d}".format(e), end=" ")
