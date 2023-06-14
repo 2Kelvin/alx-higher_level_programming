e4fa34dd10aa9ee40d096ee269a1beb72b4d71f7
2Kelvin <rocketmancodes02@gmail.com>
2023-06-14 12:18:33 -0700

    Check if key is a string and remove return from for loop

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    a_keys = list(a_dictionary)
+    if type(key) != str:
+        return a_dictionary
+    a_keys = list(a_dictionary.keys())
     for k in a_keys:
         if k == key:
             del a_dictionary[key]
-            return a_dictionary
-        else:
-            return a_dictionary
+    return a_dictionary
