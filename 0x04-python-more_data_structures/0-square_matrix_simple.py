8878602cb8a4f1866e5c9e7365bdc309a734bc0a
2Kelvin <rocketmancodes02@gmail.com>
2023-06-14 09:58:34 -0700

    Add task 0
    Use map to square each list item

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,6 +2,6 @@
 def square_matrix_simple(matrix=[]):
     bigList = []
     for eachLi in matrix:
-        sqList = map(lambda x: x ** 2, eachLi)
+        sqList = list(map(lambda x: x ** 2, eachLi))
         bigList.append(sqList)
     return bigList
