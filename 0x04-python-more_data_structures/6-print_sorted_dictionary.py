1b1a748847452891c167245c1822f8207ae1cc0b
2Kelvin <rocketmancodes02@gmail.com>
2023-06-14 11:21:38 -0700

    Add format string

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 def print_sorted_dictionary(a_dictionary):
     sortDict = dict(sorted(a_dictionary.items()))
     for dKey, dValue in sortDict.items():
-        print(dKey, dValue)
+        print("{0} {1}".format(dKey, dValue))
