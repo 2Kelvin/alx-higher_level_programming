45986475d1bfe00182062b1b3ac0cf84900b3829
2Kelvin <rocketmancodes02@gmail.com>
2023-06-14 11:59:05 -0700

    Delete dictionary key
    Use keyword del on dict[key]

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    del a_dictionary[key]
-    return a_dictionary
+    a_keys = list(a_dictionary)
+    for k in a_keys:
+        if k == key:
+            del a_dictionary[key]
+            return a_dictionary
+        else:
+            return a_dictionary
