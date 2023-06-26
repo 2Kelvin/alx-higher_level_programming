5aa4c0b7c315e91909fda10baaeaba20ec15bf1d
2Kelvin <rocketmancodes02@gmail.com>
2023-06-26 11:58:32 -0700

    Fix append on error cases

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    try:
-        divList = []
-        i = 0
-        while (i < list_length):
+    divList = []
+    i = 0
+    while (i < list_length):
+        try:
             newVl = my_list_1[i] / my_list_2[i]
+        except TypeError:
+            print("wrong type")
+            newVl = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            newVl = 0
+        except IndexError:
+            print("out of range")
+            newVl = 0
+        finally:
             divList.append(newVl)
-            i += 1
-        return divList
-    except TypeError:
-        print("wrong type")
-        divList.append(0)
-    except ZeroDivisionError:
-        print("division by 0")
-        divList.append(0)
-    except IndexError:
-        print("out of range")
-        divList.append(0)
-    finally:
-        return divList
+        i += 1
+    return divList
