63b0dd0f8584525a7f6f387fb35e4e72964d6801
2Kelvin <rocketmancodes02@gmail.com>
2023-06-27 11:22:05 -0700

    Fix errs and doc

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,7 +4,7 @@
 
 class Square:
     """Instantiate square with private attribute size
-    Args:
+    Attributes:
         size: size of one side of the square
     """
 
--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
+"""Square class that creates square instances"""
+
+
 class Square:
-    """Square class that creates square instances"""
+    """instantiating a square object
+    Attributes:
+        size: one square side
+
+    Raises:
+        TypeError: if size passed in is not an int
+        ValueError: if the size is less than 0
+    """
+
     def __init__(self, size=0):
-        """A method instantiating a square object from class Square"""
-        try:
-            if type(size) != int:
-                raise TypeError
-            elif size < 0:
-                raise ValueError
-        except TypeError:
-            print("size must be an integer")
-        except ValueError:
-            print("size must be >= 0")
-        else:
-            self.__size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
