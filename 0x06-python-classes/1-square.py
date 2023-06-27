644c7b018c7b74663462fd031e5d3605d855f0be
2Kelvin <rocketmancodes02@gmail.com>
2023-06-27 09:51:30 -0700

    Add classes folder + some tasks

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+class Square:
+    """A class Square that defines square instances"""
+    def __init__(self, size):
+        """Function that instantiates objects with private attribute size"""
+        self.__size = size
---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+class Square:
+    """Square class that creates square instances"""
+    def __init__(self, size=0):
+        """A method instantiating a square object from class Square"""
+        try:
+            if type(size) != int:
+                raise TypeError
+            elif size < 0:
+                raise ValueError
+        except TypeError:
+            print("size must be an integer")
+        except ValueError:
+            print("size must be >= 0")
+        else:
+            self.__size = size
