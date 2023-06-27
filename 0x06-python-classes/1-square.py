f0664cc09cb7496a26bb722cbeb75dc0456c642c
2Kelvin <rocketmancodes02@gmail.com>
2023-06-27 11:01:42 -0700

    Fix doc style

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""A class Square that defines square instances"""
+
+
 class Square:
-    """A class Square that defines square instances"""
+    """Instantiate square with private attribute size
+    Args:
+        size: size of one side of the square
+    """
+
     def __init__(self, size):
-        """Function that instantiates objects with private attribute size"""
         self.__size = size
