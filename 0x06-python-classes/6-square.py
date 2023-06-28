cc475e4aaa91223935893e45ef82c81b7d03faa1
2Kelvin <rocketmancodes02@gmail.com>
2023-06-28 07:37:29 -0700

    Rearrange code

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -49,19 +49,6 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
-    def my_print(self):
-        """Print the square in #"""
-        if self.__size == 0:
-            print()
-        for sp in range(self.__position[1]):
-            print()
-        for sqRow in range(self.__size):
-            for p in range(self.__position[0]):
-                print(end="")
-            for sqCol in range(self.__size):
-                print("#", end="")
-            print()
-
     @property
     def position(self):
         """Position private attribute getter"""
@@ -83,3 +70,16 @@ class Square:
         elif type(value[0]) != int or type(value[1]) != int:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
+
+    def my_print(self):
+        """Print the square in #"""
+        if self.__size == 0:
+            print()
+        for sp in range(self.__position[1]):
+            print()
+        for sqRow in range(self.__size):
+            for p in range(self.__position[0]):
+                print(end="")
+            for sqCol in range(self.__size):
+                print("#", end="")
+            print()
