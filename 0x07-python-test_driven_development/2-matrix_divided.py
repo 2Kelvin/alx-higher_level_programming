4be9b4ce3f3ab77dbdf1893e6e97a8198ddac497
2Kelvin <rocketmancodes02@gmail.com>
2023-07-04 14:52:45 -0700

    Add project folder + most tasks and some cases

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Contains matrix_divided function"""
+
+
+def matrix_divided(matrix, div):
+    """Divided all elements of a matrix
+    Args:
+        matrix: list containing lists
+        div: divisor
+
+    Raises:
+        TypeError: matrix must be a matrix (list of lists) of integers/floats,
+            each row of the matrix must be the same size & div must be a number
+        ZeroDivisionError: division by zero
+
+    Returns: a new matrix
+    """
+    for lst in matrix:
+        if len(lst) != len(matrix[0]):
+            raise TypeError("Each row of the matrix must have the same size")
+        for num in lst:
+            if type(num) != int and type(num) != float:
+                raise TypeError("matrix must
+                                be a matrix(list of lists) of integers/floats")
+    if type(div) != int and type(div) != float:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    return [list(map(lambda n: round(n/div, 2), lr)) for lr in matrix]
