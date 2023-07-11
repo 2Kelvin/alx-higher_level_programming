ed5701c76513d0f999de25d10670935e3197533f
2Kelvin <rocketmancodes02@gmail.com>
2023-07-11 08:43:38 -0700

    Fix newline error

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """Reads a UTF-8 text file and prints it to STDOUT"""
     with open(filename, "r", encoding="utf-8") as myFile:
         for eachLine in myFile.readlines():
-            print(eachLine)
+            print(eachLine, end="")
