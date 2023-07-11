b66a6dbcbb47f3bfe26145a99a3a035a3b2b62bd
2Kelvin <rocketmancodes02@gmail.com>
2023-07-11 13:38:56 -0700

    Class to dict

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """Contains function class_to_json"""
-import json
 
 
 def class_to_json(obj):
     """Returns a dictionary description of a class object"""
-    jsonObj = json.dumps(obj.__dict__)
-    return jsonObj
+    objDict = obj.__dict__
+    return objDict
