c2949c933228a8c69979f1957ef6bbff06981cce
2Kelvin <rocketmancodes02@gmail.com>
2023-08-15 16:38:57 -0700

    Fix order by

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,5 +1,3 @@
 --Say my name
 -- Lists all second_table records
-SELECT score, name FROM second_table
-  WHERE name != "" AND IS NOT NULL
-    ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
