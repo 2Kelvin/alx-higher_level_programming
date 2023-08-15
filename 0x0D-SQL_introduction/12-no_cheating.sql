135bdf93afcc64046d60bf753bc15ee8cec5a352
2Kelvin <rocketmancodes02@gmail.com>
2023-08-15 16:15:45 -0700

    Add 12 + 13

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,5 @@
+-- Cheating is bad
+-- Update Bob's score
+-- finding him using his name instead of his id
+UPDATE second_table SET score=10
+  WHERE name="Bob";
---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,4 @@
+-- Score too low
+-- Removes records whose score < 10
+DELETE FROM second_table
+  WHERE score <= 5;
