34627ebe747bf7daec58c11fb33c7dbb6d8e59a1
2Kelvin <rocketmancodes02@gmail.com>
2023-08-15 16:44:01 -0700

    Use backtick

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,5 @@
 --Say my name
 -- Lists all second_table records
-SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
+SELECT `score`, `name` FROM `second_table`
+WHERE `name` != ""
+ORDER BY `score` DESC;
