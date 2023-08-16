0e9d5b4355af0204123287bbad6f8a2a42eccfd9
2Kelvin <rocketmancodes02@gmail.com>
2023-08-16 13:30:35 -0700

    Fix #2

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,7 @@
 -- Only comedy
 -- lists all comedy shows
-SELECT tv_shows.title FROM tv_shows
-INNER JOIN tv_show_genres AS tg ON tg.show_id = t.id
-INNER JOIN tv_genres ON tg.genre_id = tv_genres.id
-WHERE tv_genres.name = "Comedy"
-ORDER BY tv_shows.title;
+SELECT t.title FROM tv_shows AS t
+JOIN tv_show_genres AS tg ON t.id = tg.show_id
+JOIN tv_genres ON tg.genre_id = tv_genres.id
+WHERE name = "Comedy"
+ORDER BY t.title ASC;
