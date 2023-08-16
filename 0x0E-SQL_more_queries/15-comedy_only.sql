6141d7ee9a776cd10e5d4dd626025efc96c2f5c3
2Kelvin <rocketmancodes02@gmail.com>
2023-08-16 13:20:35 -0700

    Fix 7 => foreign key & 15 => use INNER JOIN

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,8 +1,8 @@
 -- Only comedy
 -- lists all comedy shows
 SELECT t.title
-FROM t JOIN tv_show_genres AS tg
-ON tg.show_id = t.id JOIN tv_genres
+FROM t INNER JOIN tv_show_genres AS tg
+ON tg.show_id = t.id INNER JOIN tv_genres
 ON tg.genre_id = tv_genres.id
 WHERE tv_genres.name = "Comedy"
 ORDER BY t.title;
