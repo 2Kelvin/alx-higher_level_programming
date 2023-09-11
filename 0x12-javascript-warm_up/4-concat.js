3be9d9ba861109833ef81e440c1728251784aa36
2Kelvin <rocketmancodes02@gmail.com>
2023-09-11 15:51:46 -0700

    Fix code

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,10 +1,4 @@
 #!/usr/bin/node
 // Create a sentence
 const format = ' is ';
-if (!process.argv[2]) {
-  console.log(`undefined ${format} undefined`);
-} else if (process.argv.length === 4) {
-  console.log(`${process.argv[2]} ${format} ${process.argv[3]}`);
-} else if (process.argv.length === 3) {
-  console.log(`${process.argv[2]} ${format} undefined`);
-}
+console.log(`${process.argv[2]} ${format} ${process.argv[3]}`);
