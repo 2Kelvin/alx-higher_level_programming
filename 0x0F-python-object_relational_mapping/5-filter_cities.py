788c6048aa41c2d263117f63a5e8f6c3da30c713
2Kelvin <rocketmancodes02@gmail.com>
2023-09-15 10:13:57 -0700

    Fix errs trial #2

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -14,8 +14,8 @@ if __name__ == '__main__':
     )
     cursr = dbConnection.cursor()
     queryCities = 'SELECT cities.name FROM cities JOIN \
-        states ON cities.state_id=states.id WHERE states.name \
-             LIKE BINARY %(arg)s ORDER BY cities.id', {'arg': argv[4]}
+        states ON cities.state_id=states.id WHERE \
+            states.name=%s ORDER BY cities.id', (argv[4], )
     cursr.execute(queryCities)
     for row in cursr.fetchall():
         print(row)
