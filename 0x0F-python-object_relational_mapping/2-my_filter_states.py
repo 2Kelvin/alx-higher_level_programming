e412ea09463dffe7890468ac7b5e994357803457
2Kelvin <rocketmancodes02@gmail.com>
2023-09-15 06:39:42 -0700

    Fix error

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -12,9 +12,10 @@ if __name__ == '__main__':
         passwd=argv[2],
         db=argv[3]
     )
-    selQuery = 'SELECT * FROM states WHERE name={} ORDER BY id'.format(argv[4])
+    query = 'SELECT * FROM states WHERE BINARY name="{}" ORDER \
+        BY id'.format(argv[4])
     cursorDb = dbConnection.cursor()
-    cursorDb.execute(selQuery)
+    cursorDb.execute(query)
     for row in cursorDb.fetchall():
         print(row)
     cursorDb.close()
