576ea281ebdc818c3f1869029703049894d0e637
2Kelvin <rocketmancodes02@gmail.com>
2023-09-15 08:49:13 -0700

    Fix errs

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -13,8 +13,8 @@ if __name__ == '__main__':
         db=argv[3]
     )
     cursr = dbConnection.cursor()
-    queryCities = 'SELECT id, name, states.name FROM cities \
-        JOIN states ON state_id=states.id ORDER BY id'
+    queryCities = 'SELECT cities.id, cities.name, states.name \
+        FROM cities JOIN states ON cities.state_id=states.id ORDER BY id'
     cursr.execute(queryCities)
     for row in cursr.fetchall():
         print(row)
