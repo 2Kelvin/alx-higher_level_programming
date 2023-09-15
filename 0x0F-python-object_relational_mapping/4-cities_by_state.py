18db18f8b5cd7b730ff45461823ddea04f8504de
2Kelvin <rocketmancodes02@gmail.com>
2023-09-15 08:40:04 -0700

    Filter all cities

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Cities by states"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == '__main__':
+    dbConnection = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
+    cursr = dbConnection.cursor()
+    queryCities = 'SELECT * FROM cities ORDER BY id'
+    cursr.execute(queryCities)
+    for row in cursr.fetchall():
+        print(row)
+    cursr.close()
+    dbConnection.close()
