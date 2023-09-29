5aafcc79b15b3451de8386461b0893e7ff84669f
2Kelvin <rocketmancodes02@gmail.com>
2023-09-29 09:49:47 -0700

    tweak if statement

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,10 +5,10 @@ if __name__ == '__main__':
     import requests
     import sys
 
-    if sys.argv[1] is None:
-        q = ''
-    else:
+    if len(sys.argv[1]) >= 1:
         q = sys.argv[1]
+    else:
+        q = ''
     qData = {'q': q}
     serverUrl = 'http://0.0.0.0:5000/search_user'
     httpResponse = requests.post(serverUrl, data=qData)
