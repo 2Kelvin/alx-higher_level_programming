b14982a52da22122accd5deb84d69d26fefc058d
2Kelvin <rocketmancodes02@gmail.com>
2023-10-24 06:48:46 -0700

    no errors see:white_check_mark:

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,6 +4,6 @@ const request = require('request');
 
 const alxUrl = process.argv[2];
 request(alxUrl, (err, urlResponse, body) => {
-  console.error(`error: ${err}`);
+  if (err) console.error(`error: ${err}`);
   console.log(`code: ${urlResponse.statusCode}`);
 });
