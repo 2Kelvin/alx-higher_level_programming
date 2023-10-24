15e4bd21cd1044594107b3b2669a4735b48627fd
2Kelvin <rocketmancodes02@gmail.com>
2023-10-24 10:55:58 -0700

    am cooking these apis :male-cook:

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const apiURL = process.argv[2];
+
+request(apiURL, (err, response, body) => {
+  if (err) console.error(`error: ${err}`);
+
+  const todosList = JSON.parse(body);
+
+  for (let idUser = 1; idUser < 11; idUser++) {
+    let countCompleted = 0;
+    for (const todo of todosList) {
+      if (idUser === todo.userId) {
+        if (todo.completed === true) countCompleted++;
+      }
+    }
+    if (idUser === 1) console.log(`{ '${idUser}': ${countCompleted},`);
+    else if (idUser === 10) console.log(`  "${idUser}": ${countCompleted} }`);
+    else console.log(`  '${idUser}': ${countCompleted},`);
+  }
+});
