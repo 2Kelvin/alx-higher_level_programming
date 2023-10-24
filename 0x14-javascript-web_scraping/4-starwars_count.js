1923a62e3d596216df35b8ab0e4d9b60500d34e2
2Kelvin <rocketmancodes02@gmail.com>
2023-10-24 08:06:11 -0700

    count films

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const apiURL = process.argv[2];
+const wAntillesId = '18';
+let filmCount = 0;
+request(apiURL, (err, response, body) => {
+  if (err) console.error(`error: ${err}`);
+  const allStarWarsMovies = JSON.parse(body).results;
+
+  for (const movie of allStarWarsMovies) {
+    for (const xter in movie.characters) {
+      if (xter === wAntillesId) {
+        filmCount++;
+      }
+    }
+  }
+  console.log(filmCount);
+});
