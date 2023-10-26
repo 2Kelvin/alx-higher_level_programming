80e2ac713d7fa38f323fe404455d97f5f1c67e33
2Kelvin <rocketmancodes02@gmail.com>
2023-10-26 12:49:49 +0300

    fix 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,15 @@
 #!/usr/bin/node
 
-const request = require('request');
+const nodefs = require('fs');
 
-const alxUrl = process.argv[2];
-request(alxUrl, (err, urlResponse, body) => {
-    console.err(`error: ${err}`)
-    console.log(`code: ${urlResponse.statusCode}`);
+nodefs.readFile(process.argv[2], 'utf-8', (err, fileData) => {
+  if (err) console.log(err);
+  console.log(fileData);
 });
+
+// Future Notes for future Kelvin
+// use nodejs' `fs` module to open a file
+// use `process.argv` to grab the command line arguments passed in
+// the first arg passed is at index argv[2]
+// argv[0] is the nodejs execution path
+// and argv[1] is the js file your using
